6e5b2987080452b97f22ae7e48330e2ab6845278
pinkbluesky <alina.li@outlook.com>
2022-07-25 09:17:52 -0700

    apply diff

--- src/thermo/basics/mfes.c
+++ src/thermo/basics/mfes.c
@@ -16,40 +16,44 @@
 #include <math.h>
 #include <time.h>
 #include <string.h>
-
+#include <pthread.h>
 #include <thermo/core.h>
 #include <shared.h>
 
+typedef struct ThreadInfo {
+	pthread_t thread;
+	char **seqs;
+	int seqNum;
+	DBL_TYPE mfeSum;
+} ThreadInfo;
+
+int threadNum = 2;
+int complexity = 3;
+
+void *mfeproc(void *a);
+ 
 /* ************************************************ */
 
 int main(int argc, char *argv[])
 {
 
-  // TODO find a better way to reading in a large file  to the sequence string
-  // char seq[MAXSEQLENGTH_ADS];
-  // int seqNum[MAXSEQLENGTH_ADS + 1]; // last index contains -1 to indicate end of sequences
-
-  // DBL_TYPE strandMfes[MAXSTRANDS_ADS + 1]; // contains mfe values for each strand -- output array
-
-  printf("in mfes init vars");
   char **seqs;
-  int seqNum[MAXLINE];
+//  int seqNum[MAXLINE];
   int nStrands;
   int strandLen;
   DBL_TYPE mfeSum = 0;
 
-  int complexity = 3;
-  int tmpLength;
-  DBL_TYPE mfe;
+//  int tmpLength;
+//  DBL_TYPE mfe;
   int vs;
   char inputFile[MAXLINE];
   char outFile[MAXLINE];
   int inputFileSpecified;
   FILE *fp;
+  int seqPerThread;
+  ThreadInfo *tinfo;
 
-  dnaStructures mfeStructs = {NULL, 0, 0, 0, NAD_INFINITY};
-
-  printf("in mfes");
+//  dnaStructures mfeStructs = {NULL, 0, 0, 0, NAD_INFINITY};
 
   // Get the command line arguments
   strcpy(inputFile, "");
@@ -72,26 +76,6 @@ int main(int argc, char *argv[])
     abort();
   }
 
-  /*
-    if (!inputFileSpecified)
-    {
-      printf("Enter output file prefix: ");
-      scanf("%s", inputFile);
-      strcat(inputFile, ".in"); // Here, .in is just a placeholder
-    }
-
-    // printf("line 63 mfes.c\n");
-
-    // Read the input file
-    if (!inputFileSpecified ||
-        !ReadInputFileADSCustom(inputFile, seq, &vs, NULL, NULL, NULL))
-    {
-      if (inputFileSpecified == 0)
-        getUserInput(seq, &vs, NULL, NULL);
-      else
-        abort();
-    }
-  */
 
   // Create output file path
   strncpy(outFile, inputFile, strlen(inputFile) - 3);
@@ -100,7 +84,7 @@ int main(int argc, char *argv[])
 
   // Populate output file with basic information
   header(argc, argv, "mfes", outFile);
-  printInputs(argc, argv, NULL, vs, NULL, NULL, outFile);
+  printInputs(argc, argv, NULL, 1, NULL, NULL, outFile);
 
   if (!DO_PSEUDOKNOTS)
   {
@@ -125,7 +109,7 @@ int main(int argc, char *argv[])
     seqs[i] = (char *)malloc((strandLen + 1) * sizeof(char));
   }
 
-  printf("Allocated memory for sequences.");
+//  printf("Allocated memory for sequences.");
 
   // Read in sequences
   if (ReadInputFileADSCustom(inputFile, seqs, nStrands, strandLen, &vs, NULL, NULL, NULL) == 0)
@@ -134,9 +118,42 @@ int main(int argc, char *argv[])
     abort();
   }
 
+  seqPerThread = nStrands / threadNum;
+  if (nStrands%threadNum != 0)
+	seqPerThread++;
+
   // Get mfe for each sequence
+  tinfo = calloc(threadNum, sizeof(ThreadInfo));
+  for(int i = 0; i < threadNum; i++) {
+	ThreadInfo *ti = &tinfo[i];
+	int b, e;
+
+	b = i*seqPerThread;
+	e = b + seqPerThread;
+	if (b >= nStrands) {
+		// this can happen if we have too few strands, less than number of threads
+		threadNum = i;
+		break;
+	}
+
+	if (e > nStrands)
+		e = nStrands;
+
+	ti->seqs = &seqs[b];
+	ti->seqNum = e - b;
+	pthread_create(&ti->thread, NULL, mfeproc, ti);
+  }
+
+  for(int i = 0; i < threadNum; i++) {
+	ThreadInfo *ti = &tinfo[i];
+	pthread_join(ti->thread, NULL);
+	mfeSum += ti->mfeSum;
+  }
+
+/*
   for (int i = 0; i < nStrands; i++)
   {
+    
     // get MFE of sequence
     tmpLength = strlen(seqs[i]);
     convertSeq(seqs[i], seqNum, tmpLength);
@@ -152,6 +169,7 @@ int main(int argc, char *argv[])
 
     mfeSum += mfe;
   }
+*/
 
   // Write to output file
   fp = fopen(outFile, "a");
@@ -165,3 +183,28 @@ int main(int argc, char *argv[])
 }
 
 /* ****** */
+
+void *mfeproc(void *a) {
+	int slen;
+	ThreadInfo *ti = a;
+	int seqNum[MAXLINE];
+	DBL_TYPE mfe;
+	dnaStructures mfeStructs = {NULL, 0, 0, 0, NAD_INFINITY};
+
+	for(int i = 0; i < ti->seqNum; i++) {
+		slen = strlen(ti->seqs[i]);
+		convertSeq(ti->seqs[i], seqNum, slen);
+
+		printf("%s ", ti->seqs[i]);
+		mfe = mfeFullWithSym(seqNum, slen, &mfeStructs, complexity, DNARNACOUNT,
+                         DANGLETYPE, TEMP_K - ZERO_C_IN_KELVIN, 0,
+                         1, SODIUM_CONC, MAGNESIUM_CONC,
+                         USE_LONG_HELIX_FOR_SALT_CORRECTION);
+
+		printf("%Lf\n", mfe);
+		clearDnaStructures(&mfeStructs);
+		ti->mfeSum += mfe;
+	}
+
+	return NULL;
+}
